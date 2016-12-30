ecc55024a4fd63e4cec017b642dc3a823e955eca
Josef Rönn <josef.ronn@trustly.com>
2016-12-30 23:52:53 +0100

    Added test to check alert on new window

--- js/myjs.js
+++ js/myjs.js
@@ -1,11 +1,12 @@
 window.onload = function() {
 
   $("#arrow-down-1, #btn-about-me").click(function() {
-      $('html, body').animate({scrollTop: $("#about-me-anchor").offset().top},
+      /*$('html, body').animate({scrollTop: $("#about-me-anchor").offset().top},
         {
             duration:500,
             queue:false
-        });
+        });*/
+        window.open("test.html");
   });
 
   $("#arrow-down-2, #btn-projects").click(function() {
